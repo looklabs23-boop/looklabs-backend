0e09bc208403b6a19a12c5493fb9fe1322d80bab
looklabs23-boop <looklabs23@gmail.com>
2026-06-02 16:50:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '25mb' }));
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const REVIEWS_FILE = path.join(__dirname, 'reviews.json');
